4cfe7483e82a44234b176699454e96058fb15c1e
giorgsiap <giorgsiap@hotmail.gr>
2020-06-17 13:01:50 +0300

    Updates

---
+++ precache-manifest.7236f2265b03cfb9eba12c0b48be144a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b873dc5eea0075162b02eb85236c6491",
+    "url": "/dummies/index.html"
+  },
+  {
+    "revision": "2a993a39714e69db6aef",
+    "url": "/dummies/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0995a5f8f98c52a6c440",
+    "url": "/dummies/static/css/main.0ae257ff.chunk.css"
+  },
+  {
+    "revision": "2a993a39714e69db6aef",
+    "url": "/dummies/static/js/2.5729f633.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dummies/static/js/2.5729f633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0995a5f8f98c52a6c440",
+    "url": "/dummies/static/js/main.80fcbf25.chunk.js"
+  },
+  {
+    "revision": "8c380b13e6aa7a5b8874",
+    "url": "/dummies/static/js/runtime-main.9ac2470e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/dummies/static/media/SEGA.12a3db8b.woff"
+  }
+]);
